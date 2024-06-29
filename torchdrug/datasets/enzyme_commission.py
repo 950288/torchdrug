ec63866c75dbbfd11c97ba0f9d6cd11fc293fb6b
950288 <92421445+950288@users.noreply.github.com>
2024-06-29 14:51:32 +0800

    Update enzyme_commission.py

--- torchdrug/datasets/enzyme_commission.py
+++ torchdrug/datasets/enzyme_commission.py
@@ -8,6 +8,7 @@ from torch.utils import data as torch_data
 from torchdrug import data, utils
 from torchdrug.core import Registry as R
 
+
 @R.register("datasets.EnzymeCommission")
 @utils.copy_args(data.ProteinDataset.load_pdbs)
 class EnzymeCommission(data.ProteinDataset):
@@ -132,73 +133,3 @@ class EnzymeCommission(data.ProteinDataset):
         values = torch.ones(len(self.pos_targets[index]))
         item["targets"] = utils.sparse_coo_tensor(indices, values, (len(self.tasks),)).to_dense()
         return item
-
-@R.register("datasets.EnzymeCommissionV2")
-@utils.copy_args(data.ProteinDataset.load_pdbs)
-class EnzymeCommissionV2(EnzymeCommission):
-    """
-    A set of proteins with their 3D structures and EC numbers, which describes their
-    catalysis of biochemical reactions.
-
-    Statistics (test_cutoff=0.95):
-        - #Train: 15,011
-        - #Valid: 1,664
-        - #Test: 1,840
-
-    Parameters:
-        path (str): the path to store the dataset
-        test_cutoff (float, optional): the test cutoff used to split the dataset
-        verbose (int, optional): output verbose level
-        **kwargs
-    """
-
-    url = "https://zenodo.org/records/12589405/files/enzyme_commission%20.zip"
-    md5 = "368d4b587e78d44546d5f3633727e318"
-    processed_file = "enzyme_commission.pkl.gz"
-    test_cutoffs = [0.3, 0.4, 0.5, 0.7, 0.95]
-
-    def __init__(self, path, test_cutoff=0.95, verbose=1, **kwargs):
-        path = os.path.expanduser(path)
-        if not os.path.exists(path):
-            os.makedirs(path)
-        self.path = path
-        if test_cutoff not in self.test_cutoffs:
-            raise ValueError("Unknown test cutoff `%.2f` for EnzymeCommission dataset" % test_cutoff)
-        self.test_cutoff = test_cutoff
-        
-        zip_file = os.path.join(path, "EnzymeCommission")
-        if not os.path.exists(zip_file):
-            os.makedirs(zip_file)
-
-        zip_file = utils.download(self.url, zip_file, md5=self.md5)
-        path = os.path.join(utils.extract(zip_file))
-        pkl_file = os.path.join(path, self.processed_file)
-
-        csv_file = os.path.join(path, "nrPDB-EC_test.csv")
-        pdb_ids = []
-        with open(csv_file, "r") as fin:
-            reader = csv.reader(fin, delimiter=",")
-            idx = self.test_cutoffs.index(test_cutoff) + 1
-            _ = next(reader)
-            for line in reader:
-                if line[idx] == "0":
-                    pdb_ids.append(line[0])
-
-        if os.path.exists(pkl_file):
-            self.load_pickle(pkl_file, verbose=verbose, **kwargs)
-        else:
-            pdb_files = []
-            for split in ["train", "valid", "test"]:
-                split_path = utils.extract(os.path.join(path, "%s.zip" % split))
-                pdb_files += sorted(glob.glob(os.path.join(split_path, split, "*.pdb")))
-            self.load_pdbs(pdb_files, verbose=verbose, **kwargs)
-            self.save_pickle(pkl_file, verbose=verbose)
-        if len(pdb_ids) > 0:
-            self.filter_pdb(pdb_ids)
-
-        tsv_file = os.path.join(path, "nrPDB-EC_annot.tsv")
-        pdb_ids = [os.path.basename(pdb_file).split("_")[0] for pdb_file in self.pdb_files]
-        self.load_annotation(tsv_file, pdb_ids)
-
-        splits = [os.path.basename(os.path.dirname(pdb_file)) for pdb_file in self.pdb_files]
-        self.num_samples = [splits.count("train"), splits.count("valid"), splits.count("test")]
